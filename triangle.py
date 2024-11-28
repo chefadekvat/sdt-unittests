9fb969e1bdc3a4f0eeb24bfa252816f1d56e1fec
Chef <chefadekvat@gmail.com>
2024-11-28 03:28:12 +0300

    fixed invalid triangle

--- triangle.py
+++ triangle.py
@@ -9,4 +9,6 @@ def perimeter(a, b, c):
     '''Принимает числа a, b, c - стороны треугольника, возвращает периметр треугольника'''
     if a < 0 or b < 0 or c < 0:
         return 0
+    elif a >= b + c or b >= a + c or c >= a + c:
+        return 0
     return a + b + c
