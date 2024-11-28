59413115a82222c1a9cd6336fef3a203856cab6b
Chef <chefadekvat@gmail.com>
2024-11-28 03:24:19 +0300

    fixed below zero

--- circle.py
+++ circle.py
@@ -1,12 +1,15 @@
 import math
 
-
 def area(r):
     '''Принимает число r - радиус круга, возвращает площадь круга'''
+    if r < 0:
+        return 0
     return math.pi * r * r
 
 
 def perimeter(r):
     '''Принимает число r - радиус круга, возвращает периметр круга'''
+    if r < 0:
+        return 0
     return 2 * math.pi * r
 
